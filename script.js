ab6f483eb569311e977aaf653341523eb2839f20
LughsClues <96918584+LughsClues@users.noreply.github.com>
2022-01-19 19:37:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function computerPlay() {
+    let playNumber = Math.floor(Math.random() * 3) + 1;
+    if (playNumber === 1) {
+        return ("Rock");
+    } else if (playNumber === 2) {
+        return ("Paper");
+    } else if (playNumber === 3) {
+        return ("Scissors");
+    }
+
+}
+let playerSelection = prompt("Rock, Paper or Scissors?");
+let computerSelection = computerPlay();
+console.log(computerSelection);
+
+function game() {
+    
+
+if ((playerSelection.toLowerCase() === "rock") && computerSelection === "Scissors") {
+    return ("You Win! Rock beats Scissors.");
+} else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
+    return ("You Lose! Paper beats Rock.");
+} else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
+    return ("You Win! Paper beats Rock."); 
+} else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") {
+    return ("You Lose! Scissors beat Paper.");
+} else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
+    return ("You Lose! Rock beats Scissors.");
+} else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") {
+    return ("You Win! Scissors beat Paper.");
+} else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    return ("Its a tie. Try again.");
+} else {
+    return ("You must have misspellt something.");
+}
+ 
+}
+
+alert(game());
