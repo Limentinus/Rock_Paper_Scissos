eb6024d9fad7a1ecc523d6718a84323a1ba44935
lughsclues <cuberfreak@gmail.com>
2022-02-01 17:57:02 +0100

    Add buttons and texContent

--- script.js
+++ script.js
@@ -1,85 +1,90 @@
-let playerSelection;
-let computerSelection;
-let computerWinCount = 0;
-let playerWinCount = 0;
-// computerPlay() generates a random number between 1 and 3 and assigns each
-// number a value of either Rock, Paper or Scissors
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    results.textContent = playRound('rock', computerPlay());
+    game(playerWinCount, computerWinCount);
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    results.textContent = playRound('paper', computerPlay());
+    game(playerWinCount, computerWinCount);
+
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    results.textContent = playRound('scissors', computerPlay());
+    game(playerWinCount, computerWinCount);
+});
+
+const results = document.querySelector('#results');
+const wRes = document.querySelector('#winResult');
+
 function computerPlay() {
     let playNumber = Math.floor(Math.random() * 3) + 1;
     if (playNumber === 1) {
-        return ("Rock");
+        return ("rock");
     } else if (playNumber === 2) {
-        return ("Paper");
+        return ("paper");
     } else if (playNumber === 3) {
-        return ("Scissors");
+        return ("scissors");
     }
 
 }
 
+let computerWinCount = 0;
+let playerWinCount = 0;
+
+function game(playerWinCount, computerWinCount) {
+    if (playerWinCount === 5) {
+        wRes.textContent = `You have won the game. The score is ${playerWinCount}, ${computerWinCount}` 
+        const resetBtn = document.createElement('button');
+        resetBtn.textContent = 'Reset the Game!';
+        resetBtn.onclick = () => location.reload();
+        wRes.appendChild(resetBtn);
+
+    } else if (computerWinCount === 5) {
+        wRes.textContent = `You have lost the game. The score is ${playerWinCount}, ${computerWinCount}`;
+        computerWinCount = 0;
+        const resetBtn = document.createElement('button');
+        resetBtn.textContent = 'Reset the Game!';
+        resetBtn.onclick = () => location.reload();
+        wRes.appendChild(resetBtn);
+
+    }
+}
 
 
-function playRound() {
-
-    playerSelection = prompt("Rock, Paper or Scissors?");
-    computerSelection = computerPlay();
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") {
+function playRound(playerSelection, computerSelection) {
+    
+    
+    console.log(computerSelection);
+    if (playerSelection === "rock" && computerSelection === "scissors") {
         playerWinCount = ++playerWinCount;
         return (`You Win! The computer chose Scissors. Rock beats Scissors. The score is ${playerWinCount}, ${computerWinCount}`);
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
         computerWinCount = ++computerWinCount;
         return (`You Lose! The computer chose Paper. Paper beats Rock.The score is ${playerWinCount}, ${computerWinCount}`);
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerWinCount = ++playerWinCount;
         return (`You Win! The computer chose Rock. Paper beats Rock. The score is ${playerWinCount}, ${computerWinCount}`); 
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") {
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
         computerWinCount = ++computerWinCount;
         return (`You Lose! The computer chose Scissors. Scissors beat Paper. The score is ${playerWinCount}, ${computerWinCount}`);
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
         computerWinCount = ++computerWinCount;
         return (`You Lose! The computer chose Rock. Rock beats Scissors.The score is ${playerWinCount}, ${computerWinCount}`);
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") {
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerWinCount = ++playerWinCount;
         return (`You Win! The computer chose Paper. Scissors beat Paper. The score is ${playerWinCount}, ${computerWinCount}`);
-    } else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    } else if (playerSelection === computerSelection) {
         return (`Its a tie. Try again. The score is ${playerWinCount}, ${computerWinCount}`);
-    } else {
-        return ("You must have misspellt something.");
-    }
-    console.log(playerWinCount);
-    console.log(computerWinCount);
+    } 
+
+    
 }
 
 
-//the return message from playRound has to show up after each game
-//and winCounter has to be updated.
 
-
-
-
-
-
-
-
-// game executes playRound 5 times and then returns a message with the win or loss and the 
-// win/loss ratio
     
-function game() {
-    
-    console.log(playRound());
-   
-    console.log(playRound());
-    
-    console.log(playRound());
-    
-    console.log(playRound());
-    
-    console.log(playRound());
-
-    if (playerWinCount > computerWinCount) {
-        return (`You win the game. The score is:${playerWinCount}, ${computerWinCount}`);
-    } else if (computerWinCount > playerWinCount) {
-        return (`You lose the game.The score is:${playerWinCount}, ${computerWinCount}`);
-
-    }
-}
-console.log(game());
